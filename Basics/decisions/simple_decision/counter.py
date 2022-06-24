67c71528da1bac423aa37c08c7dc77f8e78f1308
5ntepj51 <5ntepj51@solent.ac.uk>
2022-06-24 03:54:31 +0100

    Added code to demonstrate the use counter.

---
+++ Basics/decisions/simple_decision/counter.py
@@ -0,0 +1,11 @@
+# Ask the user to enter the first whole number
+print("Please enter the first whole number")
+first_number = int(input())
+
+# Ask the user to enter the second whole number
+print("Please enter the second whole number")
+second_number = int(input())
+
+# Ask the user to enter a third whole number
+print("please enter the third whole number")
+third_number = int(input())
