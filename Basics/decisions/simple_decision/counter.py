4daa472cd6aae1bcdde6f93edd06f6729a122324
5ntepj51 <5ntepj51@solent.ac.uk>
2022-06-25 06:52:14 +0100

    Added code to demonstrate how a variable can be used as a counter

--- Basics/decisions/simple_decision/counter.py
+++ Basics/decisions/simple_decision/counter.py
@@ -9,3 +9,27 @@ second_number = int(input())
 # Ask the user to enter a third whole number
 print("please enter the third whole number")
 third_number = int(input())
+
+odd_number = 0
+even_number = 0
+
+if first_number % 2 == 0:
+    even_number = even_number + 1
+
+elif first_number % 2 == 1:
+    odd_number = odd_number + 1
+
+if second_number % 2 == 0:
+    even_number = even_number + 1
+
+elif second_number % 2 == 1:
+    odd_number = odd_number + 1
+
+if third_number % 2 == 0:
+    even_number = even_number + 1
+
+elif third_number % 2 == 1:
+    odd_number = odd_number + 1
+
+
+print(f"There were {even_number} even and {odd_number} odd numbers.")
