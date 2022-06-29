2441a4fa6cb52b3446bb38cc46347a7e8147e6d8
5ntepj51 <5ntepj51@solent.ac.uk>
2022-06-29 03:56:49 +0100

    Added code to demonstrate the use of build-in function: len, ord

--- Basics/functions/ascii_code.py
+++ Basics/functions/ascii_code.py
@@ -6,7 +6,7 @@ print("Please enter a letter")
 letter = input()
 
 if len(letter) == 1:
-    print(f"The ASCII code for {letter} is {ord(letter)} ")
+    print(f"The ASCII code for {letter} is: {ord(letter)} ")
 
 print("")
 
